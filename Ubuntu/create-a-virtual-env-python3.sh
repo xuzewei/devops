0128b7dbe73b561031c938752c826589f35b58fa
xuzewei@gmail.com <xuzewei@gmail.com>
2019-10-29 20:54:39 +0800

    add file

---
+++ Ubuntu/create-a-virtual-env-python3.sh
@@ -0,0 +1,15 @@
+# your begin
+sudo apt update
+sudo apt install virtualenv
+cd
+mkdir python3-env
+cd python3-env
+
+# create a vurtual env in python3
+virtualenv -p python3 env
+ls env/lib
+
+# active env
+source ~/python3-env/env/bin/activate
+
+#deactivate
